397f0edb42e492e2cabe393876da1e27e7787c59
.hensei <tiagoavrodrigues@gmail.com>
2025-05-31 23:24:07 +0100

    added reactive forms and authentication

--- src/app/signin/signin.page.ts
+++ src/app/signin/signin.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../services/user';
+import { SupabaseService } from '../services/supabase.service';
+import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
@@ -8,7 +11,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SigninPage implements OnInit {
 
-  constructor() { }
+  user: User;
+
+  public signinForm: FormGroup;
+  loginError: string | null = null;
+
+
+  constructor(private supabaseService: SupabaseService, private formBuilder: FormBuilder) { 
+    this.user = {
+      email: '',
+      password: '',
+      name: '',
+    };
+
+    this.signinForm = this.formBuilder.group({
+      email: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+
+  }
+
+  private async authenticate(email: string, password: string): Promise<boolean> {
+    try {
+      this.user = await this.supabaseService.getUserByEmail(email);
+      return this.user.password === password;
+    } catch (error) {
+      return false;
+    }
+  }
+
+  async onSubmit(){
+    if (this.signinForm.valid) {
+      var result = await this.authenticate(this.signinForm.get('email')?.value, this.signinForm.get('password')?.value)
+      if(result){
+        this.loginError = 'Nice :)';
+      } else {
+        this.loginError = 'Invalid email or password';
+      }
+    } else {
+      // marcar todos os campos como tocados para exibição das mensagens de erro
+      Object.values(this.signinForm.controls).forEach(control => {
+        control.markAsTouched();
+      });
+    }
+  }
 
   ngOnInit() {
   }
