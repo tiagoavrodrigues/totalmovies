c49999caeacc8fe3548d258b44080fda0dd9e1b9
.hensei <tiagoavrodrigues@gmail.com>
2025-06-01 01:21:33 +0100

    created session service

---
+++ src/app/services/session.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import { Storage }from '@ionic/storage-angular'
+import * as CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
+import { Session } from './user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SessionService {
+  session: Session | null = null;
+
+  constructor(private storage: Storage) { }
+
+  async loadSession(){
+    await this.storage.defineDriver(CordovaSQLiteDriver);
+    await this.storage.create();
+    const session = await this.storage.get('session') as Session;
+    if(session){
+      this.session = session;
+    }
+  }
+
+async createSession(session: Session) {
+    await this.storage.set('session', session);
+    const savedSession = await this.storage.get('session');
+    console.log('Session Created and Saved:', savedSession);
+    this.session = savedSession;
+    console.log('Session after save:', await this.storage.get('session'));
+}
+
+  async deleteSession(){
+    await this.storage.remove('session');
+    this.session = null;
+  }
+
+  getSession(){
+    return this.session;
+  }
+}
--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -4,3 +4,5 @@ export interface User {
     password: string;
     name: string;
 }
+
+export type Session = Pick<User, 'id' | 'email' | 'name'>;
