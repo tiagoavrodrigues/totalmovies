fc703cb7ed4c85488bf242017aa42cfadee204ff
.hensei <tiagoavrodrigues@gmail.com>
2025-06-03 08:48:19 +0100

    updated supabase service crud

--- src/app/services/supabase.service.ts
+++ src/app/services/supabase.service.ts
@@ -1,12 +1,24 @@
 import { Injectable } from '@angular/core';
 import {createClient, SupabaseClient } from '@supabase/supabase-js';
-import { User } from './user';
+import { Session, User } from './user';
 import { environment } from '../../environments/environment';
+import { RatedMovie, Review } from './ratedmovies.service';
+
+export interface Interaction{
+  id?: string,
+  userId: string,
+  movieId: number,
+  isLiked: boolean,
+  isFavorite: boolean,
+  isWatchLater: boolean
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SupabaseService {
+
+  
 
   private supabaseUrl = environment.supabaseUrl;
   private supabaseKey = environment.supabaseKey;
@@ -50,4 +62,123 @@ export class SupabaseService {
     return data;
   }
 
+
+  async getMovieInteraction(userId: string, movieId: number): Promise<Interaction | null>{
+      const { data, error } = await this.supabaseClient
+      .from('userMovieInteraction')
+      .select('*')
+      .eq('userId', userId)
+      .eq('movieId', movieId)
+      .single();
+
+    if(error) return null;
+    return data as Interaction;
+  }
+
+  async getMovieInteractions(): Promise<Interaction[] | null>{
+    const { data, error } = await this.supabaseClient
+      .from('userMovieInteraction')
+      .select('*');
+    if(error) return [];
+    return data as Interaction[];
+  }
+
+  async getMovieInteractionsByUserId(userId: string): Promise<Interaction[] | null>{
+    const { data, error } = await this.supabaseClient
+      .from('userMovieInteraction')
+      .select('*')
+      .eq('userId', userId);
+
+    if(error) return null;
+    return data as Interaction[];
+  }
+
+  async insertMovieInteraction(newInteraction: Interaction): Promise<boolean>{
+    const response = await this.getMovieInteraction(newInteraction.userId, newInteraction.movieId)
+    if(response !== null){
+      return false;
+    }
+    const { data, error } = await this.supabaseClient
+      .from('userMovieInteraction')
+      .insert({
+        userId: newInteraction.userId,
+        movieId: newInteraction.movieId,
+        isLiked: newInteraction.isLiked,
+        isFavorite: newInteraction.isFavorite,
+        isWatchLater: newInteraction.isWatchLater
+      });
+    if(error){
+      console.log(newInteraction);
+      return false;
+    }
+    return true;
+  }
+
+  async updateMovieInteraction(newInteraction: Interaction): Promise<boolean>{
+    const { data, error } = await this.supabaseClient
+      .from('userMovieInteraction')
+      .update({
+        isLiked: newInteraction.isLiked,
+        isFavorite: newInteraction.isFavorite,
+        isWatchLater: newInteraction.isWatchLater
+      })
+    .eq('id', newInteraction.id);
+
+    if (error) return false;
+    return true;
+  };
+
+  async getReviewsByMovieId(movieId: number): Promise<Review[] | null>{
+    const{ data, error } = await this.supabaseClient
+      .from('userMovieReview')
+      .select('*')
+      .eq('movieId', movieId);
+
+    if(error) return null;
+
+    return data as Review[];
+  }
+
+  async getMovieReviewByUserId(userId: string, movieId: number): Promise<Review | null>{
+    const{ data, error } = await this.supabaseClient
+      .from('userMovieReview')
+      .select('*')
+      .eq('userId', userId)
+      .eq('movieId', movieId)
+      .single();
+    if(error) return null;
+
+    return data as Review;
+  }
+
+  async insertMovieReview(newReview: Review){
+    await this.supabaseClient
+      .from('userMovieReview')
+      .insert(newReview);
+    console.log(newReview);
+
+  }
+
+  async updateMovieReviewById(newReview: Review): Promise<boolean>{
+    const{ data, error } = await this.supabaseClient
+      .from('userMovieReview')
+      .update({
+        rating: newReview.rating,
+        review: newReview.review
+      })
+      .eq('Id', newReview.id);
+    if(error) return false;
+
+    return true;
+  }
+
+  async removeMovieReviewById(reviewId: Review): Promise<boolean>{
+    const{ data, error}= await this.supabaseClient
+      .from('userMovieReview')
+      .delete()
+      .eq('id', reviewId);
+
+    if(error) return false;
+    return true;
+  }
 }
