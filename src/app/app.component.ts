9c4fcb3f8afbe9ce2beb50fc64aff46db9fd4440
.hensei <tiagoavrodrigues@gmail.com>
2025-06-03 08:53:45 +0100

    updated side menu entries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { SessionService } from './services/session.service';
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -7,13 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Sign Up', url: '/signup', icon: 'mail' },
-    { title: 'Sign In', url: '/signin', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/spam', icon: 'warning' },
+    { title: 'Genres', url: '/genres', icon: 'film' },
+    { title: 'Discover', url: '/discover', icon: 'compass' },
+    { title: 'Liked', url: '/lists/liked', icon: 'heart' },
+    { title: 'Favorites', url: '/lists/favorites', icon: 'star' },
+    { title: 'Watch Later', url: '/lists/watchlater', icon: 'time' },
   ];
-  public labels = [];//'Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  public labels = [];
+  constructor(private sessionService: SessionService, private router: Router) {}
+
+  async signOut() {
+    await this.sessionService.deleteSession();
+    this.router.navigateByUrl('/signin');
+  }
 }
